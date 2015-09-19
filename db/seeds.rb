8d073219872d1eb1c01d44d1d823e27be0c95981
manish <manishkongari@outlook.com>
2015-09-19 19:45:29 +0530

    seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ComponentType.create([{name: 'Enclosure'}, {name: 'Crimp Tool'}, {name: 'Disconnect Switch'}, {name: 'Multi-Conductor Cables'},
+                      {name: 'Circuit Protection'},{name: 'Wire Guage'},{name: 'Pilot Devices'},{name: 'Shielding'},
+                      {name: 'Power Supply'},{name: 'Connectors-Male'},{name: 'Terminal Block'},{name: 'Connectors-Female'},
+                      {name: 'Panel-Mount Receptacles'},{name: 'Pins'},{name: 'VFD'},{name: 'PLC-CPU'},{name: 'Servo/Stepper'},
+                      {name: 'Display'},{name: 'Solar Panels'},{name:'Sockets'},{name:'VFD Remote Display'},
+                      {name:'PLC Modules-Digital Inputs'},{name:'PLC Modules-Digital Outputs'},{name:'PLC Modules-Analog Inputs'},
+                      {name:'PLC Modules-Analog Outputs'},{name:'PLC Modules-Speciality'},
+                      {name: 'Power Cables'},{name: 'Battery Pack'},{name: 'Heat Shrink'},{name: 'Bypass Contactor'},{name: 'Feedback Cables'},
+                      {name: 'Nodes Transmitter'},{name: 'External Encoder'},{name: 'External Encoders'},{name: 'Nodes Receiver'},
+                      {name: 'HMI'},{name: 'HMI Communication'},{name: 'VFD/Servo/Stepper Drives'},{name: 'Safety Relay'},
+                     ])
